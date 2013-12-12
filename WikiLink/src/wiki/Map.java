dc73f5012e9c50bab8dc0587ba763ab48e4f9817
Anurag Sharma <anu231@gmail.com>
2013-12-12 23:23:07 +0530

    1. Final Commit for 12/12/12013 2. parsing the wikimarkup to html : seems to work well. The html text resembles the original page

--- WikiLink/src/wiki/Map.java
+++ WikiLink/src/wiki/Map.java
@@ -14,9 +14,12 @@ public class Map extends Mapper<LongWritable, Text, Text, Text> {
         
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         
-    	String line = value.toString();
-    	System.out.println(line);
+    	//String line = value.toString();
+    	//System.out.println(line);
+    	//System.out.println("-----------------------------------------------------------");
+    	WikiParse wp = new WikiParse();
+    	String html = wp.parseWikiMarkup(value.toString());
+    	System.out.println(html);
     	System.out.println("-----------------------------------------------------------");
-    	
-    } 
+    }
  }
